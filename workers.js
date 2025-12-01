76903c8b5db6c6c93a03c58e4f829b850f23fc8c
Loving you is like loving DJ <tonyya92@outlook.com>
2025-12-01 17:08:15 +0800

    Update workers.js

--- workers.js
+++ workers.js
@@ -10,7 +10,7 @@ import { connect } from 'cloudflare:sockets';
 export default {
   async fetch(request, env, ctx) {
     try {
-      const token = '';
+      const token = '111';
       const upgradeHeader = request.headers.get('Upgrade');
       
       if (!upgradeHeader || upgradeHeader.toLowerCase() !== 'websocket') {
